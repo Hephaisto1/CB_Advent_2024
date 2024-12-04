5af48b81d8bd098b34dc391dce9c2b924afa4bef
Hephaisto1 <hephaisto@yahoo.de>
2024-12-04 22:39:10 +0100

    Version 2:
    Erstellung von Labels und Inputs in einem BoxLayout

--- CB_Advent_2024.py
+++ CB_Advent_2024.py
@@ -1,12 +1,26 @@
 from kivy.app import App
+from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.label import Label
+from kivy.uix.textinput import TextInput
 
-class BlobApp(App):
 
+class LoginScreen(App):
     def build(self):
-        return Label(text="Hello World")
+        b = BoxLayout(orientation="vertical")
+        text_name = TextInput()
+        text_age = TextInput()
+        text_adress = TextInput()
+        label_name = Label(text = "Name: ")
+        label_age = Label(text = "Age: ")
+        label_adress = Label(text = "Adress: ")
+        b.add_widget(label_name)
+        b.add_widget(text_name)
+        b.add_widget(label_age)
+        b.add_widget(text_age)
+        b.add_widget(label_adress)
+        b.add_widget(text_adress)
+        return b
 
-#This comment is new and should change the version of the file
 
-if __name__ == "__main__":
-    BlobApp().run()
+if __name__ == '__main__':
+    LoginScreen().run()
