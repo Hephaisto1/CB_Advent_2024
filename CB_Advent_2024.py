8ac9522dd473bfa50cf8c857fbc5fb31b1656e06
Hephaisto1 <hephaisto@yahoo.de>
2024-12-08 17:41:41 +0100

    Version 3
    Erweiterung Input-Felder: Bei unzulässigen Eingaben erfolgt eine rote Hervorhebung

--- CB_Advent_2024.py
+++ CB_Advent_2024.py
@@ -4,23 +4,55 @@ from kivy.uix.label import Label
 from kivy.uix.textinput import TextInput
 
 
-class LoginScreen(App):
+
+class Window(App):
+
     def build(self):
-        b = BoxLayout(orientation="vertical")
-        text_name = TextInput()
-        text_age = TextInput()
-        text_adress = TextInput()
-        label_name = Label(text = "Name: ")
-        label_age = Label(text = "Age: ")
-        label_adress = Label(text = "Adress: ")
-        b.add_widget(label_name)
-        b.add_widget(text_name)
-        b.add_widget(label_age)
-        b.add_widget(text_age)
-        b.add_widget(label_adress)
-        b.add_widget(text_adress)
-        return b
+        self.input_field = BoxLayout(orientation="vertical")
+        self.text_name = TextInput(background_color="white",
+                                   foreground_color="black",
+                                   hint_text="Please enter your Name:",
+                                   multiline=False)
+        self.text_age = TextInput(background_color="white",
+                                   foreground_color="black",
+                                   hint_text="Please enter your Age:",
+                                   multiline=False)
+        self.text_adress = TextInput(background_color = "white",
+                                     foreground_color = "black",
+                                     hint_text = "Please enter your Adress:",
+                                     multiline=False)
+        self.text_name.bind(on_text_validate=self.name_validation)
+        self.text_age.bind(on_text_validate=self.age_validation)
+        self.text_adress.bind(on_text_validate=self.adress_validation)
+        self.input_field.add_widget(self.text_name)
+        self.input_field.add_widget(self.text_age)
+        self.input_field.add_widget(self.text_adress)
+        return self.input_field
+
+    def name_validation(self, *args):
+        if self.text_name.text.isalpha() or self.text_name.text == " ":
+            pass
+        else:
+            self.text_name.background_color = "red"
+
+    def age_validation(self, *args):
+        if self.text_age.text.isdigit() and int(self.text_age.text) > 2 and int(self.text_age.text) < 100:
+            pass
+        else:
+            self.text_age.background_color = "red"
+
+    def adress_validation(self, *args):
+        if self.text_adress.text.isalnum() or self.text_name.text == " ":
+            pass
+        else:
+            self.text_adress.background_color = "red"
 
 
 if __name__ == '__main__':
-    LoginScreen().run()
+    Window().run()
+
+"""
+To Do's: Tag 5: Die Input-Felder so gestalten, dass bei einer erneuten Eingabe/korrekten Eingabe der rote Hintergrund verschwindet
+
+
+"""
