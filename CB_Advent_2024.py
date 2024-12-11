74d089ae33e5fd27274837d20e09449e3184a849
Hephaisto1 <hephaisto@yahoo.de>
2024-12-11 20:59:36 +0100

    Version 5
    Added an "Add"-Button with functionality. Added two input and two label-widgets with text binding.

--- CB_Advent_2024.py
+++ CB_Advent_2024.py
@@ -1,44 +1,63 @@
 from kivy.app import App
-from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.relativelayout import RelativeLayout
 from kivy.uix.textinput import TextInput
+from kivy.uix.label import Label
 from kivy.storage.dictstore import DictStore
+from kivy.uix.button import Button
 
-
-
-class Window(App):
+class Christmas(App):
 
     def build(self):
+        self.field_counter = 0
         self.store = DictStore('Data_CB_advent2024.py')
-        self.input_field = BoxLayout(orientation="vertical")
+        self.input_field = RelativeLayout()
         if len(self.store.keys()) == 0:
             self.text_name = TextInput(background_color="white",
-                                   foreground_color="black",
-                                   hint_text="Please enter your Name:",
-                                   multiline=False)
+                                       foreground_color="black",
+                                       hint_text="Please enter your Name:",
+                                       multiline=False,
+                                       size_hint=(1,0.15),
+                                       pos_hint={"x":0.0,"y":0.85})
             self.text_age = TextInput(background_color="white",
                                       foreground_color="black",
                                       hint_text="Please enter your Age:",
-                                      multiline=False)
+                                      multiline=False,
+                                      size_hint=(1, 0.15),
+                                      pos_hint={"x":0.0,"y":0.7}
+                                      )
             self.text_adress = TextInput(background_color="white",
                                          foreground_color="black",
                                          hint_text="Please enter your Adress:",
-                                         multiline=False)
+                                         multiline=False,
+                                         size_hint=(1, 0.15),
+                                         pos_hint={"x":0.0,"y":0.55}
+                                         )
         else:
             self.text_name = TextInput(background_color="white",
                                        foreground_color="black",
                                        hint_text="Please enter your Name:",
                                        multiline=False,
-                                       text=self.store.get("person_data")["name"])
+                                       text=self.store.get("person_data")["name"],
+                                       size_hint=(1, 0.15),
+                                       pos_hint={"x":0.0,"y":0.85}
+                                       )
             self.text_age = TextInput(background_color="white",
                                       foreground_color="black",
                                       hint_text="Please enter your Age:",
                                       multiline=False,
-                                      text=self.store.get("person_data")["age"])
+                                      text=self.store.get("person_data")["age"],
+                                      size_hint=(1, 0.15),
+                                      pos_hint={"x":0.0,"y":0.7}
+                                      )
             self.text_adress = TextInput(background_color="white",
                                          foreground_color="black",
                                          hint_text="Please enter your Adress:",
                                          multiline=False,
-                                         text=self.store.get("person_data")["adress"])
+                                         text=self.store.get("person_data")["adress"],
+                                         size_hint=(1, 0.15),
+                                         pos_hint={"x":0.0,"y":0.55}
+                                         )
+        self.button_add = Button(text="Add",size_hint=(1, 0.25),pos_hint={"x":0.0,"y":0.0})
         self.text_name.bind(on_text_validate=self.name_validation)
         self.text_age.bind(on_text_validate=self.age_validation)
         self.text_adress.bind(on_text_validate=self.adress_validation)
@@ -48,6 +67,8 @@ class Window(App):
         self.input_field.add_widget(self.text_name)
         self.input_field.add_widget(self.text_age)
         self.input_field.add_widget(self.text_adress)
+        self.button_add.bind(on_press=self.add_field_description)
+        self.input_field.add_widget(self.button_add)
         return self.input_field
 
     def name_validation(self, *args):
@@ -72,6 +93,42 @@ class Window(App):
         self.store.put("person_data", name=self.text_name.text, age=self.text_age.text, adress=self.text_adress.text)
         print(self.store.get("person_data"))
 
+    def add_field_description(self, *args):
+        if self.field_counter == 0:
+            self.item_description = Label(color="white",
+                                          size_hint=(0.5, 0.15),
+                                          pos_hint={"x": 0.0, "y": 0.4}
+                                          )
+            self.item_field = TextInput(background_color="white",
+                                   foreground_color="black",
+                                   hint_text="Add an item:",
+                                   multiline=False,
+                                   size_hint=(1, 0.15),
+                                   pos_hint={"x":0.5,"y":0.4}
+                                    )
+            self.item_field.bind(text=self.item_description.setter("text"))
+            self.input_field.add_widget(self.item_description)
+            self.input_field.add_widget(self.item_field)
+            self.field_counter += 1
+        elif self.field_counter == 1:
+            self.item_description2 = Label(color="white",
+                                          size_hint=(0.5, 0.15),
+                                          pos_hint={"x": 0.0, "y": 0.25}
+                                          )
+            self.item_field2 = TextInput(background_color="white",
+                                        foreground_color="black",
+                                        hint_text="Add an item:",
+                                        multiline=False,
+                                        size_hint=(1, 0.15),
+                                        pos_hint={"x":0.5,"y":0.25}
+                                        )
+            self.item_field2.bind(text=self.item_description2.setter("text"))
+            self.input_field.add_widget(self.item_description2)
+            self.input_field.add_widget(self.item_field2)
+            self.field_counter += 1
+        else:
+            pass
+
 
 if __name__ == '__main__':
-    Window().run()
+    Christmas().run()
